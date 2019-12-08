fa13bc4b3e8f3af2614d2453a13a7f3b2657b84e
Ahmed Tounsi <ahmeddottounsi@gmail.com>
2019-12-09 00:27:21 +0100

    finished day 8 part 2 (loved it very much)

--- day08/solution.py
+++ day08/solution.py
@@ -10,3 +10,33 @@ layers = [pixels[i: i + image_h] for i in range(0, len(pixels), image_h)]
 least_0 = "".join(min(layers, key=lambda x: "".join(x).count('0')))
 
 print(least_0.count('1') * least_0.count('2')) # 1474
+
+## part 2
+
+def decode_pixel(i, j, depth):
+    if layers[depth][i][j] == '0' or layers[depth][i][j] == '1':
+        return layers[depth][i][j]
+
+    return decode_pixel(i, j, depth + 1)
+message = []
+for i in range(image_h):
+    layer = []
+    for j in range(image_w):
+        layer.append(decode_pixel(i, j, 0))
+    message.append(layer)
+
+print("\n".join(["".join(pixel) for pixel in message])) # JCRCB
+
+# visualising the image and saving it to a file
+from PIL import Image
+
+background = (0, 0, 0)
+
+image = Image.new("RGB", (image_w, image_h), background)
+pixels = image.load()
+
+for i in range(image_h):
+    for j in range(image_w):
+        pixels[j, i] = (255, 255, 255) if message[i][j] == '1' else (0, 0, 0)
+
+image.save("message.png")
