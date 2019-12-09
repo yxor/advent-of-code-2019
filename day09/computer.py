8f4ae490dcf285f7b4ca19ff5e2352917e65a1fb
Ahmed Tounsi <ahmeddottounsi@gmail.com>
2019-12-09 19:14:01 +0100

    finished day 9 part 2

--- day09/computer.py
+++ day09/computer.py
@@ -38,8 +38,11 @@ class IntCodeComputer:
         argc = self.argc(opcode)
         argv = self.instructs[self.pointer + 1: self.pointer + argc]
         args = [(k, v) for (k, v) in zip(argv, opcode.zfill(argc + 1)[:-2][::-1])]
-        
-        if opcode.endswith('1'):    # addition
+
+        if opcode.endswith('99'):
+            self.running = False
+
+        elif opcode.endswith('1'):   # addition
             self.instructs[self.get_position(args[2])] = self.get_value(args[0]) + self.get_value(args[1])
 
         elif opcode.endswith('2'):  # multiplication
@@ -47,7 +50,7 @@ class IntCodeComputer:
 
         elif opcode.endswith('3'):  # input
             if self.inputs.empty():
-                raise Empty("something went wrong")
+                raise Empty("something went wrong: no inputs")
             self.instructs[self.get_position(args[0])] = self.inputs.get()
 
         elif opcode.endswith('4'):  # output
@@ -57,12 +60,12 @@ class IntCodeComputer:
         elif opcode.endswith('5'):  # jump if true
             if self.get_value(args[0]) != 0:
                 jumpFlag = False
-                self.instructs[self.pointer] = self.get_value(args[1])
+                self.pointer = self.get_value(args[1])
 
         elif opcode.endswith('6'):  # jump if false
             if self.get_value(args[0]) == 0 :
                 jumpFlag = False
-                self.instructs[self.pointer] = self.get_value(args[1])
+                self.pointer = self.get_value(args[1])
 
         elif opcode.endswith('7'):  # less than
             self.instructs[self.get_position(args[2])] = 1 if self.get_value(args[0]) < self.get_value(args[1]) else 0
@@ -70,16 +73,13 @@ class IntCodeComputer:
         elif opcode.endswith('8'):  # equal
             self.instructs[self.get_position(args[2])] = 1 if self.get_value(args[0]) == self.get_value(args[1]) else 0
         
-        elif opcode.endswith('9'):
+        elif opcode.endswith('9'): # add to the relative position
             self.relative_base += self.get_value(args[0])
 
-        elif opcode.endswith('99'):
-            self.running = False
 
         if jumpFlag:
             self.pointer += argc
-        else:
-            self.pointer = self.instructs[self.pointer]
+
         
 
 
@@ -89,21 +89,27 @@ class IntCodeComputer:
         if arg[1] == self.RELATIVE_MODE:
             return arg[0] + self.relative_base
 
-        raise Exception("something went wrong")
+        raise Exception(f"something went wrong in getting the position {arg}")
 
     def get_value(self, arg : tuple) -> int:
         if arg[1] == self.POSITION_MODE:
             return self.instructs[arg[0]]
+
         if arg[1] == self.RELATIVE_MODE:
             return self.instructs[arg[0] + self.relative_base]
 
-        return arg[0]
+        if arg[1] == self.VALUE_MODE:
+            return arg[0]
         
+        raise Exception(f"something went wrong in getting the value {arg}")
+
 
     
     @staticmethod
-    def argc(code : int) -> int:
-        opcode = str(code)
+    def argc(opcode : str) -> int:
+
+        if opcode.endswith('99'):
+            return 1
         if opcode.endswith('1'):
             return 4
         if opcode.endswith('2'):
@@ -122,7 +128,5 @@ class IntCodeComputer:
             return 4
         if opcode.endswith('9'):
             return 2
-        if opcode.endswith('99'):
-            return 1
         
-        return None
+        raise Exception(f"Unvalid opcode {opcode}")
--- day09/solution.py
+++ day09/solution.py
@@ -1,8 +1,7 @@
 from computer import IntCodeComputer
 
 ## Part one
-
-HEAP_SIZE = 1000
+HEAP_SIZE = 100000
 with open("input.txt", "r") as f:
     inputs = [int(element) for element in f.read().split(',')] + [0 for _ in range(HEAP_SIZE)]
 
@@ -10,3 +9,10 @@ computer = IntCodeComputer(inputs[:])
 computer.add_input(1)
 computer.run()
 print(computer.get_output()) # 2662308295
+
+## Part two
+
+computer = IntCodeComputer(inputs[:])
+computer.add_input(2)
+computer.run()
+print(computer.get_output()) # 63441
